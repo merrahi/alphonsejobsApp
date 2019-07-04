c83ee2f72fd80d42894a4d30e2f2d7bcd8b27dcd
merrahi <merrahi@gmail.com>
2019-07-04 11:44:51 +0200

    jobs listed

--- src/Controller/Job/JobController.php
+++ src/Controller/Job/JobController.php
@@ -2,21 +2,36 @@
 // src/Controller/Job/JobController.php
 namespace App\Controller\Job;
 
+use App\Entity\Job;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-class JobController extends Controller
+
+class JobController extends AbstractController
 {
     /**
      * @Route("/list", name="list")
      */
-    public function listAction()
+    public function listAction():Response
     {
        
+        $jobs=$this->getDoctrine()->getRepository(Job::class)->findAll();
+        
+        return $this->render('job\list.html.twig',[
+            'jobs'=>$jobs
+            ]);
+    }
+    /**
+     * @Route("job/{id}",name="job_show",requirements={"id"="\d+"})
+     */
+    public function show($id)
+    {
+        $job=$this->getDoctrine()->getRepository(Job::class)->find($id);
 
-        return new Response(
-            '<html><body>Lucky number: 10 </body></html>'
-        );
+        return $this->render('job\show.html.twig',[
+            'job'=>$job
+        ]);
+
     }
 }
--- src/Fixtures/Job/JobFixtures.php
+++ src/Fixtures/Job/JobFixtures.php
@@ -13,13 +13,14 @@ class JobFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
+        $logos=['alphonse.png','sensiolabs.png','bouygues.jpg','huawei.png','orange.png'];
         // create 5 Job! Bam!
         for ($i = 0; $i < 5; $i++) {
             $job = new Job();
             $job->settype("Developpement web".$i);
             $job->setCompany("Alphonse ".$i);  
-            $job->setLogo('logo '.$i.' for the job '.$i);   
-            $job->setUrl('url '.$i.' for the job '.$i);    
+            $job->setLogo($logos[$i]);    
+            $job->setUrl($logos[$i]);    
             $job->setPosition('Evry');
             $job->setLocation('Ile de france');
             $job->setDescription('bababababab'.$i);
